e7e04a60a6da398d085cd35a6411de649ebede21
Christian Eichert <c@zp1.net>
2024-11-25 01:05:49 +0100

    function mob_str 	modified:   src/funktionen/F.php

--- src/funktionen/F.php
+++ src/funktionen/F.php
@@ -117,7 +117,7 @@ function isMobile(): bool
 }
 
 const mobile_geraete = ["android", "avantgo", "blackberry", "bolt", "boost", "cricket", "docomo", "fone", "hiptop", "mini", "mobi", "palm", "phone", "pie", "tablet", "up\.browser", "up\.link", "webos", "wos", "iphone", "ipad"];
-function mob_str()
+function mob_str(): string
 {
     return implode("|", mobile_geraete);
 }
